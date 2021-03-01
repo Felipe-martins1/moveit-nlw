3682ae8b0de8eed0a99bb5f7d9b7eb3c9080bf44
Felipe <Felipe.dom.martins@gmail.com>
2021-03-01 10:23:33 -0300

    ExperienceBar and LevelUpModal animations

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -13,7 +13,11 @@ export function ExperienceBar() {
     <header className={styles.experienceBar}>
       <span>0 xp</span>
       <div>
-        <div style={{ width: `${percentToNextLevel}%` }} />
+        <div
+          style={{
+            width: `${percentToNextLevel}%`,
+          }}
+        />
         {percentToNextLevel > 0 && (
           <span
             className={styles.currentExperience}
